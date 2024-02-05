add93a68378c0683dea0b3c6d1c65cd83b9e1e76
BhagSingh <122624070+BhagSingh@users.noreply.github.com>
2024-02-05 11:36:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 // Use NodeJS built-in function to read text file contents asynchronously
 const fs = require('fs');
 
+// Specify the file path
 const filePath = 'file.txt';
 
-
+// Use the 'readFile' function to read the contents of the text file
 fs.readFile('file.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading the file:',err);
         return;
     }
-    // Display the content using DOM methods
-    document.addEventListener('DOMContentLoaded', () => {
-        // Assuming you have an HTML element with id 'fileContent' to display the paragraph
-        const fileContentElement = document.getElementById('fileContent');
-
-        // Update the innerText of the HTML element with the content of the text file
-        fileContentElement.innerText = data;
-    });
-    // // Display the content of the text file
-    // console.log('File Content:');
-    // console.log(data);
+    
+    // Display the content of the text file
+    console.log('File Content:');
+    console.log(data);
 });
